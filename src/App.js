7d8b255868c90ffbc6ccc155e9832861c88da6df
Joel Mussman <jmussman@smallrock.net>
2022-08-15 15:02:32 -0400

    Added second Git demo message

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-		Hello, World!
+		Hello, World! Welcome to the second Git demo.
       </header>
     </div>
   );
