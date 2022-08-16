38c8beeaf1d7c890b3d46706fdd11e048fce9650
Joel Mussman <jmussman@smallrock.net>
2022-08-15 22:55:05 -0400

    Added Feature1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Feature1 from './Feature1';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 		Hello, World! Welcome to the Git demo.
+		<Feature1 />
       </header>
     </div>
   );
---
+++ src/Feature1.js
@@ -0,0 +1,9 @@
+function Feature1() {
+  return (
+    <div>
+		Feature1
+    </div>
+  );
+}
+
+export default Feature1;
